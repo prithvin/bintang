9d05e8d0a0ea26fb6c0f834f4388edb27602f1c1
Prithvi Narasimhan <narasimhan.prithvi@gmail.com>
2022-04-16 15:15:31 -0700

    Court reservations

--- src/courts.ts
+++ src/courts.ts
@@ -1,15 +1,18 @@
 import fetch from 'cross-fetch';
 
 
-const COURT_URL = 'https://app.bukza.com/api/timetables/getSettings/16509/15296';
+const SITE_LIST_ENDPOINT = 'https://app.bukza.com/api/timetables/getSettings/16509/15296';
+const COURT_LIST_ENDPOINT = ({ userId, siteId }: { userId: number, siteId: number }) => (
+  `https://app.bukza.com/api/resource-groups/getClientCatalog/${String(userId)}/${String(siteId)}?t=${new Date().getTime()}`
+);
 
-const fetchCourts = async (headers: Record<string, string>) => {
-  const resp = await fetch(`${COURT_URL}?t=${new Date().getTime()}`, {
+const fetchSites = async (headers: Record<string, string>) => {
+  const resp = await fetch(`${SITE_LIST_ENDPOINT}?t=${new Date().getTime()}`, {
     headers,
     body: null,
     method: 'GET',
   });
-  const timetablePayload: {
+  const { items, account: { userId } }: {
     account: { userId: number },
     timetable: { id: number, resourceGroupId: number },
     items: {
@@ -17,9 +20,39 @@ const fetchCourts = async (headers: Record<string, string>) => {
       name: string,
     }[],
   } = await resp.json();
-  console.log(timetablePayload.items.map(({ name }) => (name)));
-
+  return {
+    sites: items.map(({ name, resourceGroupId: id }) => ({ name, id })),
+    userId,
+  };
 };
 
+const fetchCourts = async (headers: Record<string, string>) => {
+  const { sites, userId } = await fetchSites(headers);
+  // avoid rate limiting with reduce
+  const siteBySiteInfo = await sites.reduce(async (acc, { id: siteId }) => {
+    const resp = await fetch(COURT_LIST_ENDPOINT({ siteId, userId }), {
+      headers,
+      body: null,
+      method: 'GET',
+    });
+    const { name, items }: {
+      name: string,
+      items: {
+        resourceId: number, name: string,
+      }[],
+    } = await resp.json();
+    return [
+      ...(await acc),
+      {
+        name,
+        items: items.map(({ resourceId, name }) => ({
+          resourceId, name,
+        })),
+        siteId,
+      }
+    ];
+  }, Promise.resolve([]));
+  console.log(JSON.stringify(siteBySiteInfo, null, 4));
+}
 
 export default fetchCourts;
