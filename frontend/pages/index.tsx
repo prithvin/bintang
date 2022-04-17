6def0f803cf6d7fd80ae77252aaf30bca9eba5ea
Prithvi Narasimhan <narasimhan.prithvi@gmail.com>
2022-04-16 17:00:58 -0700

    Add react bootstrap + save availabity info in frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,19 +1,63 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import cx from 'classnames';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Button } from 'react-bootstrap';
+import availabilities from './availabilities.json';
+import server from './server.json';
 
 const Home: NextPage = () => {
+  const [refetching, setRefetching] = useState(false);
+  useEffect(() => {
+    if (refetching) {
+      axios.get(`${server.url}/`)
+        .then(function (response) {
+          // handle success
+          console.log(response);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+    }
+  }, [refetching]);
   return (
-    <div>
+    <div className="bg-light h-100 py-3 px-2">
       <Head>
         <title>Bintang Scraping Application</title>
         <meta name="description" content="Bintang Scraping Application" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl font-bold underline">
-        Bintang Frontend
-      </h1>
+      {server.url == null && (
+        <div className="w-100 bg-warning p-5 display-6 text-center">
+          Please turn on your backend to get started
+          <Button
+            variant="danger"
+            className="px-3 py-2 d-block mx-auto mt-5"
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            Try again
+          </Button>
+        </div>
+      )}
+      {server.url != null && (
+        <Button
+          variant={availabilities == null ? 'danger' : 'primary'}
+          className="px-3 py-2 d-block mx-auto my-3"
+        >
+          {availabilities == null && (
+            'Fetch Available Blocks'
+          )}
+          {availabilities != null && (
+            'Reload Available Blocks'
+          )}
+        </Button>
+      )}
     </div>
-  )
+  );
 }
 
 export default Home
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,51 @@
 import _ from 'lodash';
+import fs from 'fs';
+import ngrok from 'ngrok';
+import fastifyApp from 'fastify';
 import moment from 'moment';
 import fetchAuthHeader from './headerFetch';
 import fetchCourts from './courts';
 import fetchAvailabilities from './availabilities';
 
 const targetDate = moment('04/17/2022', 'MM/DD/YYYY').toDate();
-const scrapeBintang = async () => {
+
+const fastify = fastifyApp({ logger: true })
+
+fastify.get('/', async (request, reply) => {
   const headers = await fetchAuthHeader();
   const { courts, userId, timeTableId } = await fetchCourts(headers);
   const availableBlocks = await fetchAvailabilities({ targetDate, courts, headers, userId });
   const courtResourceMapping = _.keyBy(_.flatten(courts
     .map(({ items }) => items)), 'resourceId');
-  console.log(JSON.stringify({
-    userId,
-    timeTableId,
-    blocks: availableBlocks.map(({ resourceId, blocks }) => ({
-      resourceId, blocks, name: courtResourceMapping[resourceId].name,
-    }))
-  }));
-}
+  await fs.promises.writeFile(
+    __dirname + '/../frontend/pages/availabilities.json',
+    JSON.stringify({
+      userId,
+      timeTableId,
+      blocks: availableBlocks.map(({ resourceId, blocks }) => ({
+        resourceId, blocks, name: courtResourceMapping[resourceId].name,
+      }))
+    }),
+  );
 
-scrapeBintang();
+  return { success: true };
+})
+
+const SERVER_PORT = 5000;
+
+const start = async () => {
+  try {
+    await fastify.listen(SERVER_PORT)
+    const url = await ngrok.connect();
+    await fs.promises.writeFile(
+      __dirname + '/../frontend/pages/server.json',
+      JSON.stringify({
+        url,
+      }),
+    );
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
